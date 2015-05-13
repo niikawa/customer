a3605178930787915e5963a5d71bd0a320b55071
niikawa <vxc.pc.niikawa@gmail.com>
2015-05-13 02:32:32 +0000

    commit by niikawa

--- server.js
+++ server.js
@@ -37,15 +37,12 @@ mssql.connect(config, function(err) {
 //  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
 //
 var router = express();
-var server = http.createServer(router);
-
 router.use(express.static(path.resolve(__dirname, 'client')));
 
-var customer = require('./api/customer');
+var customer = require('./api/customer,js');
 router.get('/customer/:id/', customer.getById);
 
-
-
+var server = http.createServer(router);
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("server listening at", addr.address + ":" + addr.port);
