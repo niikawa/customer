06854f3a6c7759001bdfb7a566aaea0cc32303d3
niikawa <vxc.pc.niikawa@gmail.com>
2015-05-18 03:04:18 +0000

    niikawa

--- client/scripts/services/azure.js
+++ client/scripts/services/azure.js
@@ -17,7 +17,7 @@ azureServices.factory("Azure", ['$resource', '$http','Shared',
                 }
             };
             var url = 'https://ussouthcentral.services.azureml.net/workspaces/bb07a48a7dce4617b33d3a20dd4e2604/services/c43a39e8bf454632a00268bbae4c580f/execute?api-version=2.0&details=true';
-            url += param.serialize() + '&callback=JSON_CALLBACK';
+            url += '&callback=JSON_CALLBACK';
 
             var promise = $http.jsonp(url, config).success(function(data, status, headers, config)
             {
