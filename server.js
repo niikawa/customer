99365088e1703c1ed225a9b02d24d8420b9edfe5
niikawa <vxc.pc.niikawa@gmail.com>
2015-05-20 00:51:25 +0000

    niikawa

--- server.js
+++ server.js
@@ -68,14 +68,14 @@ var param = {
 var options = {
   uri: 'https://ussouthcentral.services.azureml.net/workspaces/bb07a48a7dce4617b33d3a20dd4e2604/services/82d002728e7842f5828b114a21511835/execute?api-version=2.0&details=true',
   form: JSON.stringify(param),
-  //body: param,
   headers: {
       'Content-Type': 'application/json',    
       'Authorization': 'bearer ' + key
   },
+  json: true
   // auth: {
-  //   'user': 'new.river0314@gmail.com',
-  //   'pass' : 'taku@0314',
+  //   'user': '',
+  //   'pass' : '',
   //   'sendImmediately': false,
   //   'bearer': key},
 };
