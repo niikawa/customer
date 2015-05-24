f1dad034e55f946b818f91a01f22f33fe327bcd4
niikawa <vxc.pc.niikawa@gmail.com>
2015-05-24 15:27:13 +0000

    niikawa

--- client/scripts/directives/d3/lineDirective.js
+++ client/scripts/directives/d3/lineDirective.js
@@ -63,114 +63,87 @@ myApp.directive('lineChart', ['d3Service', '$parse', function (d3Service, $parse
               .scale(y)
               .orient("left");
             
-            var line = d3.svg.line()
-              .x(function(d){ return x(d.date); })
-              .y(function(d){ return y(d.value); });
-            
-
-            // 描画
-            scope.data.forEach(function(d){
-              d.date = parseDate(d.date);
-              d.value = +d.value;
-            });
-
-            x.domain(d3.extent(scope.data, function(d){ return d.date; }));
-            y.domain(d3.extent(scope.data, function(d){ return d.price; }));
-
-svg.append("g")
-  .attr("class", "x axis")
-  .attr("transform", "translate(0, " + ( size.height - margin.top - margin.bottom ) + ")")
-  .call(xAxis);
-
-svg.append("g")
-  .attr("class", "y axis")
-  .call(yAxis)
-  .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", ".7em")
-    .style("text-anchor", "end")
-    .text("値の単位");
-
-svg.append("path")
-  .datum(data)
-  .attr("class", "line")
-  .attr("d", line);
 
             // // $watchリスナの登録解除関数格納用.
-            // var watched = {}; 
+            var watched = {}; 
 
-            // // (Angular) $parseでCollection要素へのアクセサを確保しておく.
-            // var getId = $parse(scope.key || 'Id');
-            // var getValue = $parse(scope.valueProp || 'value');
-            // var getLabel = $parse(scope.label || 'name');
+            // (Angular) $parseでCollection要素へのアクセサを確保しておく.
+            var getId = $parse(scope.key || 'Id');
+            var getValue = $parse(scope.valueProp || 'value');
+            var getLabel = $parse(scope.label || 'name');
 
-            // (Angular) Collectionの要素変動を監視.
-//             scope.$watchCollection('data', function()
-//             {
-//                 if(!scope.data)
-//                 {
-//                   return;
-//                 }
+            //(Angular) Collectionの要素変動を監視.
+            scope.$watchCollection('data', function()
+            {
+                if(!scope.data)
+                {
+                  return;
+                }
 
-//                 // (D3 , Angular) data関数にて, $scopeとd3のデータを紐付ける.
-// //                var dataSet = svg.selectAll('g.data-group').data(scope.data, getId);
+                var line = d3.svg.line()
+                  .x(function(d){ return x(d.date); })
+                  .y(function(d){ return y(d.price); });
+                
+                x.domain(d3.extent(scope.data, function(d){ return d.date; }));
+                y.domain(d3.extent(scope.data, function(d){ return d.price; }));
 
-// //                var dataSet = svg.selectAll('g').data(scope.data, getId);
-//                 x.domain(d3.extent(scope.data, function(d) { return d.date; }));
-//                 y.domain(d3.extent(scope.data, function(d) { return d.price; }));
+                // データを入力ドメインとして設定
+                // 同時にextentで目盛りの単位が適切になるようにする
                 
-//                 // 線の定義
-//                 var line = d3.svg.line()
-//                     .x(function(d) { return x(d.date); })
-//                     .y(function(d) { return y(d.price); });
-                    
-//                 // データを入力ドメインとして設定
-//                 // 同時にextentで目盛りの単位が適切になるようにする
+                // (D3) enter()はCollection要素の追加に対応.
+                // var createdGroup = scope.data.enter().append('g').each(function(d)
+                // {
+                //     // (Angular) Collection要素毎の値に対する変更は、$watchで仕込んでいく.
+                //     var self = d3.select(this);
+                //     watched[getId(d)] = scope.$watch(function()
+                //     {
+                //         return getValue(d);
+                //     },
+                //     function(v)
+                //     {
+                //         self.select('rect').attr('width', v);
+                //     });
+                // });
                 
-//                 // (D3) enter()はCollection要素の追加に対応.
-//                 // var createdGroup = scope.data.enter().append('g').each(function(d)
-//                 // {
-//                 //     // (Angular) Collection要素毎の値に対する変更は、$watchで仕込んでいく.
-//                 //     var self = d3.select(this);
-//                 //     watched[getId(d)] = scope.$watch(function()
-//                 //     {
-//                 //         return getValue(d);
-//                 //     },
-//                 //     function(v)
-//                 //     {
-//                 //         self.select('rect').attr('width', v);
-//                 //     });
-//                 // });
-                
-//                 // (D3) exit()はCollection要素の削除に対応.
-//                 // scope.data.exit().each(function(d)
-//                 // {
-//                 //     // (Angular) $watchに登録されたリスナを解除して、メモリリークを防ぐ.
-//                 //     // var id = getId(d);
-//                 //     // watched[id]();
-//                 //     // delete watched[id];
-//                 // }).remove();
+                // (D3) exit()はCollection要素の削除に対応.
+                // scope.data.exit().each(function(d)
+                // {
+                //     // (Angular) $watchに登録されたリスナを解除して、メモリリークを防ぐ.
+                //     // var id = getId(d);
+                //     // watched[id]();
+                //     // delete watched[id];
+                // }).remove();
 
-//                 // (D3) Collection要素変動の度に再計算する箇所.
-//                 scope.data.forEach(function(d)
-//                 {
-//                     d.date = parseDate(d.date);
-//                     d.price = +d.price;
-//                     console.log(d.date);
-//                 });
+                // (D3) Collection要素変動の度に再計算する箇所.
+                // 描画
+                scope.data.forEach(function(d)
+                {
+                    d.date = parseDate(d.date);
+                    d.price = +d.price;
+                    console.log(d.date);
+                });
                 
-//                 svg.append("g").attr("class", "x axis").call(xAxis).attr("transform", "translate(0, " + ( size.height - margin.top - margin.bottom ) + ")");
-//                 svg.append("g").attr("class", "y axis").call(yAxis).append("text").attr("y", 6).attr("dy", ".71em").style("text-anchor", "end").text(getLabel);
+                svg.append("g")
+                  .attr("class", "x axis")
+                  .attr("transform", "translate(0, " + ( size.height - margin.top - margin.bottom ) + ")")
+                  .call(xAxis);
                 
-//                 // path要素をsvgに表示し、折れ線グラフを設定
-//                 svg.append("path")
-//                     .datum(scope.data)
-//                     .attr("class", "line")
-//                     .attr("stroke", "black")    // 線の色を指定
-//                     .attr("fill", "none")
-//                     .attr("d", line);            
-//             });
+                svg.append("g")
+                  .attr("class", "y axis")
+                  .call(yAxis)
+                  .append("text")
+                    .attr("transform", "rotate(-90)")
+                    .attr("y", 6)
+                    .attr("dy", ".7em")
+                    .style("text-anchor", "end")
+                    .text("値の単位");
+                
+                svg.append("path")
+                  .datum(data)
+                  .attr("class", "line")
+                  .attr("d", line);
+                
+            });
         }
     };
 }]);
