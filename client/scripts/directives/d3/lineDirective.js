a1ab0f2c10451b85cc520acd821fc5e52c02111f
niikawa <vxc.pc.niikawa@gmail.com>
2015-05-27 06:24:00 +0000

    niikawa

--- client/scripts/directives/d3/lineDirective.js
+++ client/scripts/directives/d3/lineDirective.js
@@ -80,7 +80,7 @@ myApp.directive('lineChart', ['d3Service', '$parse', '$window', function (d3Serv
             
                 //TODO 
                 var xAxis = 
-                  d3.svg.axis().scale(x).orient("bottom").tickFormat(d3.time.format("%Y/%m").innerTickSize(-10));
+                  d3.svg.axis().scale(x).orient("bottom").innerTickSize(-10).tickFormat(d3.time.format("%Y/%m"));
 
                 var yAxis = 
                   d3.svg.axis().scale(y).orient("left");
