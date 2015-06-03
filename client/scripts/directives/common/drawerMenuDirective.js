73d9a6c40b2dafa38503c03b3ca3fbd1734403f7
niikawa <vxc.pc.niikawa@gmail.com>
2015-06-03 08:13:40 +0000

    menu test

--- client/scripts/directives/common/drawerMenuDirective.js
+++ client/scripts/directives/common/drawerMenuDirective.js
@@ -4,18 +4,23 @@ myApp.directive('drawerMenuDirective', function()
     return {
         restrict: 'E',
         scope: {menuList: '='},
-        template: '<div class="buble-drawer-menu"></div>',
+        template: '<nav></nav>',
         link: function (scope, element, attrs) 
         {
+            element.append('<a class="buble-drawer-menu"><i class="fa fa-bar-chart"></i></a>');
+            
+            
+            
             element.on('click', function()
             {
-                element.after('<div class="buble-drawer-menu"></div>');
+                
             });
             if (void 0 === scope.menuList) return;
             
             for(var i = 0 ;scope.menuList.length; i++)
             {
-                var add = '';
+                
+                element.after('<div class="buble-drawer-menu"></div>');
             }
         }
     };
