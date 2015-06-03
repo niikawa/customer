5f98f54c37b9b4b52e74e181a88d7c8cb57976da
niikawa <vxc.pc.niikawa@gmail.com>
2015-06-03 09:14:15 +0000

    menu test

--- client/scripts/directives/common/drawerMenuDirective.js
+++ client/scripts/directives/common/drawerMenuDirective.js
@@ -7,20 +7,16 @@ myApp.directive('drawerMenuDirective', function()
         template: '<nav></nav>',
         link: function (scope, element, attrs) 
         {
-            element.append('<a class="buble-drawer-menu"><i class="fa fa-bar-chart"></i></a>');
-            
-            
+            element.append('<label class="menu-open-button" for="menu-open"><span class="hamburger hamburger-1"></span><span class="hamburger hamburger-2"></span><span class="hamburger hamburger-3"></span></label>');
             
             element.on('click', function()
             {
-                
+                element.after('<div class="buble-drawer-menu" style="transform: translate3d(50px,20px,10px);"></div>');
             });
             if (void 0 === scope.menuList) return;
             
             for(var i = 0 ;scope.menuList.length; i++)
             {
-                
-                element.after('<div class="buble-drawer-menu"></div>');
             }
         }
     };
