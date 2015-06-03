2f276d526d948bbb6d92c67ab576f529e67b8dab
niikawa <vxc.pc.niikawa@gmail.com>
2015-06-03 01:24:52 +0000

    get test

--- client/scripts/directives/common/mapDirective.js
+++ client/scripts/directives/common/mapDirective.js
@@ -1,26 +1,67 @@
 /**
+ * マップを生成する
+ * 
  * 
  */
 var myApp = angular.module('myApp');
 myApp.directive('mapDirective', [ 'Utility' ,function(Utility){
     return {
-        restrict: 'E',
+        restrict: 'A',
         scope:{
             mapClass: '@',
         },
         link: function (scope, element, attrs) 
         {
+            if (void 0 === element.attrs('id')) return false;
+            
             element.addClass(scope.mapClass); 
-            if (! navigator.geolocation)
+            
+            if (!navigator.geolocation)
             {
-                //つかえないメッセージをどこにだそう？
                 Utility.errorSticky('ご利用のブラウザでは位置情報を取得できません。');
                 return false;
             }
-            return true;
+
+            navigator.geolocation.getCurrentPosition(createGoogleMap, error);
             
+            function createGoogleMap(position)
+            {
+                var map = new GMaps({
+                    div: '#' + element.attrs('id'),
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                });
+                map.drawOverlay({
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                    content: '<div class="overlay">現在地</div>'
+                });            
+            }
             
-            
+            function error(error)
+            {
+                var message = "";
+                
+                switch (error.code) {
+                
+                  // 位置情報が取得できない場合
+                  case error.POSITION_UNAVAILABLE:
+                    message = "位置情報の取得ができませんでした。";
+                    break;
+                
+                  // Geolocationの使用が許可されない場合
+                  case error.PERMISSION_DENIED:
+                    message = "位置情報取得の使用許可がされませんでした。";
+                    break;
+                
+                  // タイムアウトした場合
+                  case error.PERMISSION_DENIED_TIMEOUT:
+                    message = "位置情報取得中にタイムアウトしました。";
+                    break;         
+                }
+                
+                Utility.errorSticky(message);
+            }
             
         }
     };
