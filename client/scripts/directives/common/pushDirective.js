c66763f66d337a3ceb24aff72fd282e404048066
niikawa <vxc.pc.niikawa@gmail.com>
2015-06-17 14:05:36 +0000

    test

--- client/scripts/directives/common/pushDirective.js
+++ client/scripts/directives/common/pushDirective.js
@@ -26,9 +26,11 @@ myApp.directive('pushDirective', function(){
                 }
                 else if ("string" === typeof(scope.active) || "number" === typeof(scope.active))
                 {
-                    if (scope.pushInfo[scope.activekey] == scope.active) 
+                    console.log(scope.pushInfo[scope.activekey]);
+                    console.log(scope.active);
+                    if (scope.pushInfo[scope.activekey] == scope.active)
                     {
-                        scope.pushInfo.isPush = scope.active;
+                        scope.pushInfo.isPush = true;
                         setActive = true;
                     }
                 }
