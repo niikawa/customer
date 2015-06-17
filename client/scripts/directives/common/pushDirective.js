48149d03feaaa47552b138c933a89f1eacf9a36f
niikawa <vxc.pc.niikawa@gmail.com>
2015-06-17 13:41:10 +0000

    test

--- client/scripts/directives/common/pushDirective.js
+++ client/scripts/directives/common/pushDirective.js
@@ -18,7 +18,7 @@ myApp.directive('pushDirective', function(){
             if (void 0 !== scope.active)
             {
                 scope.pushInfo.isPush = scope.active();
-                console.log(scope.pushInfo.isPush);
+                element.removeClass('push-active');
             }
             else
             {
