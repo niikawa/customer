da6abe818b93a43bb1ca38e258dbb73c4a3fa988
niikawa <vxc.pc.niikawa@gmail.com>
2015-06-17 14:58:13 +0000

    test

--- client/scripts/controllers/userControlCtrl.js
+++ client/scripts/controllers/userControlCtrl.js
@@ -75,6 +75,7 @@ function ($scope, $routeParams, User, Role, Utility, Location)
                 same: function (modelValue, viewValue)
                 {
                     var val = modelValue || viewValue;
+                    if (val.length === 0) return true;
                     
                     User.resource.isSameMailAddress({user_id: $scope.user.user_id, mailaddress: val}).$promise.then(function(response)
                     {
