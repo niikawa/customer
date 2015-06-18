73bf4b2fe2e9076bbe05f05e762573277718f058
niikawa <vxc.pc.niikawa@gmail.com>
2015-06-18 15:13:29 +0000

    test

--- client/scripts/controllers/userControlCtrl.js
+++ client/scripts/controllers/userControlCtrl.js
@@ -19,12 +19,12 @@ function ($scope, $routeParams, User, Role, Utility, Location)
         $scope.user = {};
         $scope.pageTitle = pageProp.title;
         $scope.showPassword = true;
-        $scope.passwordEdit = 1;
+        $scope.passwordEdit = true;
         $scope.pageType = pageProp.type;
 
         if (2 === pageProp.type)
         {
-            $scope.passwordEdit = 0;
+            $scope.passwordEdit = false;
             $scope.showPassword = false;
             var id = parseInt($routeParams.id, 10);
             User.resource.get({id: id}).$promise.then(function(response)
