e37a33c71c8de65fe04f5c79ad568f77182154c6
niikawa <vxc.pc.niikawa@gmail.com>
2015-06-20 16:10:03 +0000

    test

--- client/scripts/directives/conditionDirective.js
+++ client/scripts/directives/conditionDirective.js
@@ -8,35 +8,46 @@ myApp.directive('conditionDirective', function(){
         template: '指定した値' +
                 '<select ng-model="selectedCondition" ng-options="item as item.name for item in items">'+
                 'ものを条件とする'+
-                '<div ng-if="isOneInput"></div>'+
-                '<div ng-if="isTwoInput"></div>'
+                '<div ng-if="isOneInput"><input type="text"></div>'+
+                '<div ng-if="isTwoInput"><input type="text">～<input type="text"></div>'
                   ,
         scope:{
-            
+            appendModel: '='
         },
         link: function (scope, element, attrs) 
         {
+            var showOneInput = function()
+            {
+                scope.isOneInput = true;
+                scope.isTwoInput = false;
+            }
+            
+            var showTwoInput = function()
+            {
+                scope.isOneInput = false;
+                scope.isTwoInput = true;
+            }
+            
             scope.items = [
-                {name: 'に等しい', value: 1},
-                {name: '以上', value: 2},
-                {name: '以下', value: 3},
-                {name: 'を超える', value: 4},
-                {name: '未満', value: 5},
-                {name: 'の間', value: 6},
-                {name: 'を含む', value: 7},
-                {name: 'を含まない', value: 8},
-                {name: 'から始まる', value: 9},
-                {name: 'で終わる', value: 10},
-                {name: 'を一部に持つ', value: 11},
+                {name: 'に等しい', value: 1, execute: showOneInput},
+                {name: '以上', value: 2, execute: showOneInput},
+                {name: '以下', value: 3, execute: showOneInput},
+                {name: 'を超える', value: 4, execute: showOneInput},
+                {name: '未満', value: 5, execute: showOneInput},
+                {name: 'の間', value: 6, execute: showTwoInput},
+                {name: 'を含む', value: 7, execute: showOneInput},
+                {name: 'を含まない', value: 8, execute: showOneInput},
+                {name: 'から始まる', value: 9, execute: showOneInput},
+                {name: 'で終わる', value: 10, execute: showOneInput},
+                {name: 'を一部に持つ', value: 11, execute: showOneInput},
             ];
             scope.isOneInput = false;
             scope.isTwoInput = false;
             
             element.find('select').on('change', function()
             {
-                console.log('chage event');
                 console.log(scope.selectedCondition);
-                
+                scope.selectedCondition.execute();
             });
         }
     };
