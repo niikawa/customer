5b8aeecdf4bc491c61b4c616f489475f522dbc5f
niikawa <vxc.pc.niikawa@gmail.com>
2015-06-21 05:51:56 +0000

    test

--- client/scripts/controllers/query.js
+++ client/scripts/controllers/query.js
@@ -85,6 +85,7 @@ function ($scope, Shared, Query, Location)
         {
             v.column.inputType = Query.getContentsByColumsType(v.column.type);
         });
+        $scope.conditions = Shared.get('queryColumns');;
     };
     
     $scope.removeItem = function(index)
