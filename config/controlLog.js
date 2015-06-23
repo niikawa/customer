887500fb0f361af119b03a89912b83897580af64
niikawa <vxc.pc.niikawa@gmail.com>
2015-06-23 06:58:00 +0000

    commit

--- config/controlLog.js
+++ config/controlLog.js
@@ -5,25 +5,25 @@ var controlLog = {
         var data = {};
         switch(controlType)
         {
-            case 0:
+            case 1:
                 data = {show_flag: 1, detail: '[ログインしました]'};
                 break;
-            case 1: 
+            case 2: 
                 data = {show_flag: 1, detail: '[ユーザーの登録をしました]'};
                 break;
-            case 2: 
+            case 3: 
                 data = {show_flag: 1, detail: '[ユーザーの更新をしました]'};
                 break;
-            case 3: 
+            case 4: 
                 data = {show_flag: 1, detail: '[クエリを作成しました]'};
                 break;
-            case 4: 
+            case 5: 
                 data = {show_flag: 1, detail: '[クエリを更新しました]'};
                 break;
-            case 5: 
+            case 6: 
                 data = {show_flag: 1, detail: '[セグメントを作成しました]'};
                 break;
-            case 6: 
+            case 7: 
                 data = {show_flag: 1, detail: '[セグメントを更新しました]'};
                 break;
     
