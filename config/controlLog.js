e1ddd75d52194dccf1edf66b3a56748c177d478a
niikawa <vxc.pc.niikawa@gmail.com>
2015-06-23 06:56:34 +0000

    commit

--- config/controlLog.js
+++ config/controlLog.js
@@ -1,31 +1,34 @@
-module.exports = function get(controlType)
-{
-    var data = {};
-    switch(controlType)
+var controlLog = {
+    
+    get: function(controlType)
     {
-        case 0:
-            data = {show_flag: 1, detail: '[ログインしました]'};
-            break;
-        case 1: 
-            data = {show_flag: 1, detail: '[ユーザーの登録をしました]'};
-            break;
-        case 2: 
-            data = {show_flag: 1, detail: '[ユーザーの更新をしました]'};
-            break;
-        case 3: 
-            data = {show_flag: 1, detail: '[クエリを作成しました]'};
-            break;
-        case 4: 
-            data = {show_flag: 1, detail: '[クエリを更新しました]'};
-            break;
-        case 5: 
-            data = {show_flag: 1, detail: '[セグメントを作成しました]'};
-            break;
-        case 6: 
-            data = {show_flag: 1, detail: '[セグメントを更新しました]'};
-            break;
-
+        var data = {};
+        switch(controlType)
+        {
+            case 0:
+                data = {show_flag: 1, detail: '[ログインしました]'};
+                break;
+            case 1: 
+                data = {show_flag: 1, detail: '[ユーザーの登録をしました]'};
+                break;
+            case 2: 
+                data = {show_flag: 1, detail: '[ユーザーの更新をしました]'};
+                break;
+            case 3: 
+                data = {show_flag: 1, detail: '[クエリを作成しました]'};
+                break;
+            case 4: 
+                data = {show_flag: 1, detail: '[クエリを更新しました]'};
+                break;
+            case 5: 
+                data = {show_flag: 1, detail: '[セグメントを作成しました]'};
+                break;
+            case 6: 
+                data = {show_flag: 1, detail: '[セグメントを更新しました]'};
+                break;
+    
+        }
+        return data;
     }
-    return data;
-
 };
+module.exports = controlLog;
