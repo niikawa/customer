ef27e2cfdaf2c15b197df769267f9c460dd8e860
niikawa <vxc.pc.niikawa@gmail.com>
2015-06-24 06:25:43 +0000

    commit

--- client/scripts/services/user.js
+++ client/scripts/services/user.js
@@ -43,7 +43,7 @@ uesrServices.factory("User", ['$resource','$http','$q','Utility',
                 },
                 function rejected()
                 {
-                    return $q.reject('not exists');
+                    return true;
                });
             }
         };
