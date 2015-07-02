91ae3dc7ad06d3667a180bc5db3d9a74fe623112
niikawa <vxc.pc.niikawa@gmail.com>
2015-07-02 09:15:51 +0000

    commit

--- api/query.js
+++ api/query.js
@@ -120,7 +120,7 @@ exports.execute = function(req, res)
     
 //    var sql = "SELECT count(1) AS count FROM " + tableList.join(',') + ' WHERE ' + req.body.condition;
     
-    var append = createSQL(req.body.showConditions, request);
+    var append = createSQL(req.body.conditionList, request);
     var sql = "SELECT count(1) AS count FROM " + tableList.join(',') + ' WHERE ' + append;
     
     
