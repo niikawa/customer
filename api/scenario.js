4a09a6bf5604785a38a5ac64bc16d0443f721ef8
niikawa <vxc.pc.niikawa@gmail.com>
2015-07-07 10:15:02 +0000

    commit

--- api/scenario.js
+++ api/scenario.js
@@ -49,9 +49,9 @@ exports.getById = function(req, res)
  */
 exports.getAll = function(req, res)
 {
-    var col = "scenario_id, FORMAT(update_date, 'yyyy/MM/DD') AS update_date, scenario_name, " +
-                "CASE approach WHEN 1 THEN '対象' WHEN 0 THEN '対象外' ELSE '未設定' END AS approach, " +
-                "CASE status WHEN 1 THEN '有効' WHEN 0 THEN '無効' ELSE '未設定' END AS status";
+    var col = "scenario_id, FORMAT(update_date, 'yyyy/MM/dd') AS update_date, scenario_name, " +
+                "CASE approach WHEN 1 THEN N'対象' WHEN 0 THEN N'対象外' ELSE N'未設定' END AS approach, " +
+                "CASE status WHEN 1 THEN N'有効' WHEN 0 THEN N'無効' ELSE N'未設定' END AS status";
                 
     var where = "delete_flag = 0 AND scenario_type = @scenario_type";
     var order = "scenario_id";
@@ -149,8 +149,8 @@ function update(req, res)
  */
 exports.remove = function(req, res)
 {
-    var user_id = req.params.id;
-    model.removeById(user_id, function(err, data)
+    var id = req.params.id;
+    model.removeById(id, function(err, data)
     {
         if (err.length > 0)
         {
