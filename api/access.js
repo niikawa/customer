a639c3ec43df5b7049270b09aadbf9b730778c5e
niikawa <vxc.pc.niikawa@gmail.com>
2015-07-09 10:38:47 +0000

    commit

--- api/access.js
+++ api/access.js
@@ -15,13 +15,17 @@ util.inherits(log, Core);
 
 var model = new log();
 
-exports.getAll = function(req, res)
+exports.getDayAll = function(req, res)
 {
     var col = "T1.log_id, FORMAT(T1.create_date, 'yyyy-MM-dd hh:mm:ss') as date, T1.user_id, T1.detail, T2.name";
     var table = "T_LOG T1 INNER JOIN M_USER T2 ON T1.user_id = T2.user_id";
-    var where = "T1.delete_flag = 0 AND T2.delete_flag = 0";
+    var where = "T1.create_date BETWEEN @start AND @end AND T1.delete_flag = 0 AND T2.delete_flag = 0";
     var order = "T1.log_id DESC";
     var qObj = model.getQueryObject(col, table, where, '', order);
+    var start = req.body.day + '00:00:00';
+    var end = req.body.day + '23:59:59';
+    qObj.request.input('start', model.db.NVarChar, start);
+    qObj.request.input('end', model.db.NVarChar, end);
     
     model.select(qObj, qObj.request, function(err, data)
     {
