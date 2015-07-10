e487b3480da6ccb9ab12205afbdb6edbd65dd8f1
niikawa <vxc.pc.niikawa@gmail.com>
2015-07-10 06:50:06 +0000

    commit

--- client/scripts/directives/conditionDirective.js
+++ client/scripts/directives/conditionDirective.js
@@ -12,7 +12,7 @@ myApp.directive('conditionDirective', function(){
                 '指定した値' +
                 '<select ng-model="mySlected" class="form-control" ng-options="item as item.name for item in selectItems" ng-required="true"></select>'+
                 'ものを条件とする'+
-                '<div ng-if="isOneInput"><input type="text" class="form-control" ng-model="conditionAppend.condition.value1" ng-blur="check(conditionAppend)" ng-required="true">'+
+                '<div ng-if="isOneInput"><input type="text" class="form-control" ng-model="conditionAppend.condition.value1" ng-keyup="check(conditionAppend)" ng-required="true">'+
                 '</div>'+
                 '<div ng-if="isTextArea"><textarea class="form-control" ng-model="conditionAppend.condition.value1" ng-required="true"></textarea></div>'+
                 '<div ng-if="isTwoInput"><input type="text" class="form-control" ng-model="conditionAppend.condition.value1" ng-required="true">～<input type="text" class="form-control" ng-model="conditionAppend.condition.value2" ng-required="true"></div>'
