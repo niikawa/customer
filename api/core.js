72046a839e58ca56a04e26e47be4a55eca07614c
niikawa <vxc.pc.niikawa@gmail.com>
2015-07-12 04:41:19 +0000

    commit

--- api/core.js
+++ api/core.js
@@ -263,7 +263,7 @@ core.prototype.insertLog = function(userId, controlType, appendString, replace, 
         {
             repString = appendString.replace("$1", replace);
         }
-        detail += '<br>' + repString;
+        detail += "\\n" + repString;
     }
     
     if (void 0 !== data)
