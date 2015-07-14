87718c3969f11a38dd0cf58f286665cc5a9dc77e
niikawa <vxc.pc.niikawa@gmail.com>
2015-07-14 08:38:12 +0000

    commit

--- api/bug.js
+++ api/bug.js
@@ -19,23 +19,23 @@ exports.getByConditon = function(req, res)
 {
     var request = model.getRequest();
     
-    var col = "T1.id, FORMAT(T1.create_by, 'yyyy-MM-dd hh:mm:ss') as create_by, T1.status, T1.type, T1.category, T1.title, T1.contents";
-    col += " T2.name, T2.role_id";
+    var col = "T1.id, FORMAT(T1.create_by, 'yyyy-MM-dd hh:mm:ss') as create_by, T1.status, T1.type, T1.category, T1.title, T1.contents, ";
+    col += "T2.name, T2.role_id";
     var tableName = "T_DEMAND_BUG T1 INNER JOIN M_USER T2 ON T1.create_by = T2.user_id";
     var where = '';
     if (req.body.hasOwnProperty('status')) 
     {
-        where += "status = @status AND";
+        where += "T1.status = @status AND";
         request.input('status', model.db.Int, req.body.status);
     }
     
     if (req.body.hasOwnProperty('type')) 
     {
-        where += "type = @type AND";
+        where += "T1.type = @type AND";
         request.input('type', model.db.Int, req.body.type);
     }
 
-    where = " delete_flag = 0";
+    where = " T1.delete_flag = 0";
 
     var order = "id DESC";
     var qObj = model.getQueryObject(col, tableName, where, '', order);
