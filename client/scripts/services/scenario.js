92c9b176f3ce52a3d02cedb851324cd4454cb478
niikawa <vxc.pc.niikawa@gmail.com>
2015-07-14 00:04:07 +0000

    commit

--- client/scripts/services/scenario.js
+++ client/scripts/services/scenario.js
@@ -11,7 +11,7 @@ scenarioServices.factory("Scenario", ['$resource','$http','$q','Utility',
         
         scenarioServices.intervalConditionList = [
             1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
-            21,22,23,24,25,26,27,28,28,30,31
+            21,22,23,24,25,26,27,28,28,30,31, '最終日'
             ];
 
         scenarioServices.resource = $resource('/scenario/:type/:id/', {id: '@id'},
@@ -145,50 +145,6 @@ scenarioServices.factory("Scenario", ['$resource','$http','$q','Utility',
             return doc;
         };
         
-        scenarioServices.mock = function()
-        {
-            var getList = function(type)
-            {
-                if (1 === type)
-                {
-                    return [
-                        {scenario_id: 1, scenario_name:'スケジュール型シナリオ1', update_date: '2015-06-10', status: '利用中'}
-                    ];
-                }
-                else if (2 === type)
-                {
-                    return [
-                        {scenario_id: 2, scenario_name:'トリガー型シナリオ1', update_date: '2015-06-10', status: '利用中'}
-                    ];
-                }
-            };
-            
-            var data = {
-                scenario : [
-                    {scenario_type: 'schedule', scenario_name:'スケジュール型シナリオ', regist_num: 1, regist_max: 99},
-                    {scenario_type: 'trigger', scenario_name:'トリガー型シナリオ', regist_num: 1, regist_max: 99}
-                ],
-            
-                executePlanScenario : [
-                    {scenario_id: 1, scenario_name:'スケジュール型シナリオ1', update_date: '2015-06-10', status: '利用中'},
-                    {scenario_id: 2, scenario_name:'トリガー型シナリオ1', update_date: '2015-06-11', status: '利用中'},
-                    {scenario_id: 3, scenario_name:'トリガー型シナリオ2', update_date: '2015-06-12', status: '停止中'},
-                    {scenario_id: 4, scenario_name:'スケジュール型シナリオ2', update_date: '2015-06-13', status: '利用中'}
-                ],
-                
-                priorities: [
-                    {priorities:1, scenario_id: 1, scenario_name:'スケジュール型シナリオ1', scenario_type:'schedule', update_date: '2015-06-10', status: '利用中'},
-                    {priorities:2, scenario_id: 2, scenario_name:'トリガー型シナリオ1', scenario_type:'trigger', update_date: '2015-06-11', status: '利用中'},
-                    {priorities:3, scenario_id: 3, scenario_name:'トリガー型シナリオ2', scenario_type:'trigger', update_date: '2015-06-12', status: '停止中'},
-                    {priorities:4, scenario_id: 4, scenario_name:'スケジュール型シナリオ2', scenario_type:'schedule', update_date: '2015-06-13', status: '利用中'}
-                ],
-                
-                getList: getList,
-
-            };
-            return data;
-        };
-
         return scenarioServices;
     }
 ]);
