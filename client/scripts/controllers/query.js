ee400048f23ab2295ae3857bdf6a445011eb8d3b
niikawa <vxc.pc.niikawa@gmail.com>
2015-07-16 04:59:53 +0000

    commit

--- client/scripts/controllers/query.js
+++ client/scripts/controllers/query.js
@@ -71,7 +71,7 @@ function ($scope, $routeParams, Shared, Query, Location, Utility)
         $scope.showSelectedColumnsBox = $scope.selectColumns.length > 0;
         $scope.conditions = [];
         $scope.isShowEditMessage = false;
-        Query.setReturnUrl($scope.returnUrl);
+        Query.setReturnURL($scope.returnUrl);
         Shared.setRoot('query');
     }
     
