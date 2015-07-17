a06e86596fc0a3fc67ece57f720d57c6d3ce2610
niikawa <vxc.pc.niikawa@gmail.com>
2015-07-17 07:06:18 +0000

    commit

--- server.js
+++ server.js
@@ -32,9 +32,11 @@ mssql.connect(config, function(err) {
 });
 
 var router = express();
+var cookieParser = require('cookie-parser');
+
 router.set('secretKey', 'ix-cpm-forazure');
 router.set('cookieSessionKey', 'sid');
-router.use(express.cookieParser(router.get('secretKey')));
+router.use(cookieParser(router.get('secretKey')));
 router.use(express.session({secret: router.get('secretKey')}));
 
 router.use(express.static(path.resolve(__dirname, 'client')));
