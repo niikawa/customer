31039d68e1bd8c9adde82a25ef9c9623da21196d
niikawa <vxc.pc.niikawa@gmail.com>
2015-07-20 07:09:26 +0000

    commit

--- helper/fileHelper.js
+++ helper/fileHelper.js
@@ -1,26 +1,6 @@
 var fs = require("fs");
 var moment = require("moment");
 
-var fileHelper = 
-{
-    /**
-     * 出力先
-     */
-    output: "",
-    /**
-     * セパレータ
-     */
-    separator: ",",
-    /**
-     * 拡張子
-     */
-    extension: ".csv",
-    /**
-     * 1つの配列に格納される要素の最大数
-     */
-    maxListNum: 100000
-};
-
 function createHeaderList(row)
 {
     var headerRowList = [];
@@ -67,133 +47,150 @@ function createSpecifiedRowList(row, outputList)
     return rowList;
 }
 
-fileHelper.setSeparator = function(s)
+var fileHelper = 
 {
-    this.separator = s;
-};
-
-fileHelper.setExtension = function(ex)
-{
-    this.extension = ex;
-};
-
-
-fileHelper.read = function()
-{
+    /**
+     * 出力先
+     */
+    output: "",
+    /**
+     * セパレータ
+     */
+    separator: ",",
+    /**
+     * 拡張子
+     */
+    extension: ".csv",
+    /**
+     * 1つの配列に格納される要素の最大数
+     */
+    maxListNum: 100000,
     
-};
+    /**
+     * 
+     */
+     setSeparator: function(s)
+     {
+        this.separator = s;
+     },
 
-/**
- * Example:
- *          option = 
- *          {
- *              fileName: "sample",
- *              separator: ",",
- *              extension: "csv",
- *              max: 10,
- *              outputCol: {name: name}
- *          }
- * 
- * @param {object} データベースから取得した結果
- * @param {object} option 
- *                      path : ファイル出力パス
- *                      fileName : 指定しない場合はYYYYMMDhhmmss
- *                      separator : 指定しない場合はカンマ
- *                      extension : 指定しない場合はcsv
- *                      max : 指定しない場合は制限なし
- *                      outputCol : 出力対象を指定する
- */
-fileHelper.write = function(dbData, option, callback)
-{
-    console.log(dbData);
-    console.log(option);
-    if (option.hasOwnProperty("separator")) this.setSeparator(option.separator);
-    if (option.hasOwnProperty("extension")) this.setExtension(option.extension);
-    
-    var isHeader = true;
-    if (option.hasOwnProperty('isHeader'))
+    setExtension: function(ex)
     {
-        isHeader = option.isHeader;
-    }
-    
-    var fileName = "";
-    if (option.hasOwnProperty("fileName"))
-    {
-        fileName = option.fileName;
-    }
-    else
-    {
-        fileName = moment().format('YYYYMMDDhhmmss');
-    }
+        this.extension = ex;
+    },
 
-    if (option.hasOwnProperty("path"))
+    /**
+     * Example:
+     *          option = 
+     *          {
+     *              fileName: "sample",
+     *              separator: ",",
+     *              extension: "csv",
+     *              max: 10,
+     *              outputCol: {name: name}
+     *          }
+     * 
+     * @param {object} データベースから取得した結果
+     * @param {object} option 
+     *                      path : ファイル出力パス
+     *                      fileName : 指定しない場合はYYYYMMDhhmmss
+     *                      separator : 指定しない場合はカンマ
+     *                      extension : 指定しない場合はcsv
+     *                      max : 指定しない場合は制限なし
+     *                      outputCol : 出力対象を指定する
+     */
+    write: function(dbData, option, callback)
     {
-        this.output = option.path + "/" + fileName;
-    }
-    
-    var isRemove = false;
-    if (option.hasOwnProperty("outputCol"))
-    {
-        isRemove = option.outputCol.length > 0;
-    }
-    
-    if (isHeader)
-    {
-        var headerRowList = [];
-        if (isRemove)
+        console.log(dbData);
+        console.log(option);
+        if (option.hasOwnProperty("separator")) this.setSeparator(option.separator);
+        if (option.hasOwnProperty("extension")) this.setExtension(option.extension);
+        
+        var isHeader = true;
+        if (option.hasOwnProperty('isHeader'))
         {
-            headerRowList = createSpecifiedHeaderList(dbData[0], option.outputCol);
+            isHeader = option.isHeader;
+        }
+        
+        var fileName = "";
+        if (option.hasOwnProperty("fileName"))
+        {
+            fileName = option.fileName;
         }
         else
         {
-            headerRowList = createHeaderList(dbData[0]);
+            fileName = moment().format('YYYYMMDDhhmmss');
         }
-
-        if (0 === headerRowList.length)
-        {
-            var err = 
-            {
-                status: "510",
-                message: "It does not create a header",
-                parameters: {data: dbData, option: option}
-            };
-            callback(err, "");
-        }
-            
-        var headerRow = headerRowList.join(this.separator);
-        fs.writeFileSync(this.output, headerRow, 'utf8');
-    }
     
-    var dataNum = dbData.length;
-    var splitNum = (dataNum < this.maxListNum) ? 0 : Math.ceil(dataNum / this.maxListNum);
-    var row = {};
-    var rowList = [];
-    var rowString = "\n";
-    if (isRemove)
-    {
-        // for (var index = 0; index < splitNum; index++)
-        // {
-            for (var rowNum = 0; rowNum < dataNum; rowNum++)
+        if (option.hasOwnProperty("path"))
+        {
+            this.output = option.path + "/" + fileName;
+        }
+        
+        var isRemove = false;
+        if (option.hasOwnProperty("outputCol"))
+        {
+            isRemove = option.outputCol.length > 0;
+        }
+        
+        if (isHeader)
+        {
+            var headerRowList = [];
+            if (isRemove)
             {
-                row = dbData[rowNum];
-                rowList = createSpecifiedRowList(row, option.outputCol);
-                rowString += rowList.join(this.separator) + "\n";
+                headerRowList = createSpecifiedHeaderList(dbData[0], option.outputCol);
             }
-            fs.appendFileSync(this.output, rowString, 'utf8');
-        // }
-    }
-    else
-    {
-        // for (var index = 0; index < splitNum; index++)
-        // {
-            for (var rowNum = 0; rowNum < dataNum; rowNum++)
+            else
             {
-                row = dbData[rowNum];
-                rowList = createRowList(row);
-                rowString += rowList.join(this.separator) + "\n";
+                headerRowList = createHeaderList(dbData[0]);
             }
-            fs.appendFileSync(this.output, rowString, 'utf8');
-        // }
-    }
-    callback(null, {output: this.output, fileName: fileName});
+    
+            if (0 === headerRowList.length)
+            {
+                var err = 
+                {
+                    status: "510",
+                    message: "It does not create a header",
+                    parameters: {data: dbData, option: option}
+                };
+                callback(err, "");
+            }
+                
+            var headerRow = headerRowList.join(this.separator);
+            fs.writeFileSync(this.output, headerRow, 'utf8');
+        }
+        
+        var dataNum = dbData.length;
+        var splitNum = (dataNum < this.maxListNum) ? 0 : Math.ceil(dataNum / this.maxListNum);
+        var row = {};
+        var rowList = [];
+        var rowString = "\n";
+        if (isRemove)
+        {
+            // for (var index = 0; index < splitNum; index++)
+            // {
+                for (var rowNum = 0; rowNum < dataNum; rowNum++)
+                {
+                    row = dbData[rowNum];
+                    rowList = createSpecifiedRowList(row, option.outputCol);
+                    rowString += rowList.join(this.separator) + "\n";
+                }
+                fs.appendFileSync(this.output, rowString, 'utf8');
+            // }
+        }
+        else
+        {
+            // for (var index = 0; index < splitNum; index++)
+            // {
+                for (var rowNum = 0; rowNum < dataNum; rowNum++)
+                {
+                    row = dbData[rowNum];
+                    rowList = createRowList(row);
+                    rowString += rowList.join(this.separator) + "\n";
+                }
+                fs.appendFileSync(this.output, rowString, 'utf8');
+            // }
+        }
+        callback(null, {output: this.output, fileName: fileName});
+    }    
 };
