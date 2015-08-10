d8c36a630e8b0f607e815835dcef85d2369381f4
niikawa <vxc.pc.niikawa@gmail.com>
2015-08-10 06:33:11 +0000

    commit

--- server.js
+++ server.js
@@ -28,7 +28,9 @@ mssql.connect(config, function(err) {
   }
 });
 
+
 var router = express();
+console.log(router.get('env'));
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var bodyParser = require('body-parser');
