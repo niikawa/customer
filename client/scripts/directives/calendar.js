51bf1625b27d718784c5fb7eccc4bf382b060c24
niikawa <vxc.pc.niikawa@gmail.com>
2015-08-11 08:47:19 +0000

    commit

--- client/scripts/directives/calendar.js
+++ client/scripts/directives/calendar.js
@@ -3,27 +3,22 @@ myApp.directive('calendarDirective', function()
 {
     return {
         restrict: 'E',
-        scope: {bind: '='},
         templateUrl: '../../partials/calendar.html',
         transclude: true,
         link: function (scope, element, attrs) 
         {
             scope.showCircle = false;
-            element.on("mouseorver", function()
-            {
-                console.log("mouseorver");
-                scope.showCircle = true;
-            });
-            
             element.on("mouseenter", function()
             {
                 console.log("mouseenter");
+                element.find("i").addClass("show");
                 scope.showCircle = true;
             })
             
             element.on("mouseleave", function()
             {
                 console.log("mouseleave");
+                element.find("i").remnoveClass("show");
                 scope.showCircle = false;
             });
             
