1d07293b08cdbee5615f0b7e6d81a2a3b2e9eb37
niikawa <vxc.pc.niikawa@gmail.com>
2015-08-14 03:38:01 +0000

    commit

--- client/scripts/directives/calendar.js
+++ client/scripts/directives/calendar.js
@@ -90,7 +90,9 @@ myApp.controller('CalendarCtrl',['$scope','Calendar', 'Utility', function ($scop
         isDisabled = true;
         var now = $scope.year + '/' + $scope.month;
         var yearMonth = Utility.addMonth(now, 1).format("YYYY-MM");
-        var params = yearMonth.split("/");
+        console.log(yearMonth);
+        var params = yearMonth.split("-");
+
         Calendar.resource.month({year:params[0], month: params[1]}).$promise.then(function(response)
         {
             isDisabled = false;
