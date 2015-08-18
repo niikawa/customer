4a1f81e8fb802e0e394196681818024ccf801ac7
niikawa <vxc.pc.niikawa@gmail.com>
2015-08-18 06:35:36 +0000

    commit

--- api/bug.js
+++ api/bug.js
@@ -20,11 +20,11 @@ exports.getByConditon = function(req, res)
 {
     var request = model.getRequest();
     var col = "T1.id, FORMAT(T1.create_date, 'yyyy-MM-dd hh:mm:ss') as create_date, T1.resolve, T1.type, T1.category, T1.title, T1.contents, T2.name";
-    col += ", count(T3.demand_bug_comment_id)";
+    col += ", count(T3.demand_bug_comment_id) as comment_count";
     var tableName = "T_DEMAND_BUG T1 INNER JOIN M_USER T2 ON T1.create_by = T2.user_id LEFT JOIN T_DEMAND_BUG_COMMENT T3 ON T1.id = T3.demand_bug_id";
     var where = '';
     
-    var groupBy = "T1.id, FORMAT(T1.create_date, 'yyyy-MM-dd hh:mm:ss') as create_date, T1.resolve, T1.type, T1.category, T1.title, T1.contents, T2.name";
+    var groupBy = "T1.id, T1.create_date, T1.resolve, T1.type, T1.category, T1.title, T1.contents, T2.name";
     
     if (req.body.hasOwnProperty('resolve') && null !== req.body.resolve) 
     {
