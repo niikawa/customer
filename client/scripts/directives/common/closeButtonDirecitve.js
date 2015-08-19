2412fe62adedc13c2302cad3a5579f69641c4785
niikawa <vxc.pc.niikawa@gmail.com>
2015-08-19 00:12:04 +0000

    commit

--- client/scripts/directives/common/closeButtonDirecitve.js
+++ client/scripts/directives/common/closeButtonDirecitve.js
@@ -1,11 +1,27 @@
 var myApp = angular.module('myApp');
+myApp.controller('CloseButtonCtrl',['$scope', 'Modal', function ($scope, Modal)
+{
+    this.modal = function()
+    {
+        $scope.modalParam = 
+        {
+            title: "",
+            message: "",
+            isExecute: false,
+        };
+        $scope.modalInstance = Modal.open($scope.modalParam, "partials/modal/message.html");
+    };
+    
+}]);
+
 myApp.directive('closeButtonDirecitve', function(Shared, Modal)
 {
     return {
         restrict: 'E',
         scope: {execute: '&', params: "="},
+        controller:'CloseButtonCtrl',
         template: '<button ng-if="isShowMine" class="close-button" ng-click="click()"><i class="fa fa-times"></i></button>',
-        link: function (scope, element, attrs) 
+        link: function (scope, element, attrs, closeButtonCtrl) 
         {
             if (3 != Shared.get("role"))
             {
@@ -18,6 +34,8 @@ myApp.directive('closeButtonDirecitve', function(Shared, Modal)
             
             scope.click = function()
             {
+                console.log(closeButtonCtrl);
+                
                 scope.modalParam = 
                 {
                     title: "",
