6e2aed07428512fd94683a15cafa749e4aa89b33
niikawa <vxc.pc.niikawa@gmail.com>
2015-08-19 07:45:32 +0000

    commit

--- server.js
+++ server.js
@@ -41,8 +41,8 @@ router.use(express.static(path.resolve(__dirname, 'client')));
 router.use(express.static(path.resolve(__dirname, 'files')));
 router.use(bodyParser.json());
 //router.use(bodyParser.urlencoded());
-var multer  = require('multer')
-router.use(multer({ dest: './uploads/'}))
+var multer  = require('multer');
+router.use(multer({ dest: 'uploads/'}))
 
 
 var auth = require('./api/auth');
