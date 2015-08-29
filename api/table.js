8ee435b0ea5fe66d51f1526de01c668640080b60
niikawa <vxc.pc.niikawa@gmail.com>
2015-08-29 03:59:31 +0000

    commit

--- api/table.js
+++ api/table.js
@@ -30,8 +30,8 @@ exports.getTables = function(req, res)
         
         console.log(tableList);
         //表示対象のテーブルが持つカラム一覧を取得する
-        var columnSql = "SELECT t.name table_name ,c.name column_name ,sc.data_type ,sc.character_maximum_length max_lengt ,cast(ep.value as nvarchar) commnet" +
-                        "FROM sys.tables AS t, ,sys.columns AS c, sys.extended_properties AS ep, INFORMATION_SCHEMA.COLUMNS sc " +
+        var columnSql = "SELECT t.name table_name ,c.name column_name ,sc.data_type ,sc.character_maximum_length max_lengt ,cast(ep.value as nvarchar) commnet " +
+                        "FROM sys.tables t, sys.columns c, sys.extended_properties ep, INFORMATION_SCHEMA.COLUMNS sc " +
                         "WHERE t.name = @tableName AND t.object_id = c.object_id AND c.object_id = ep.major_id AND c.column_id = ep.minor_id AND t.name = sc.table_name AND sc.COLUMN_NAME = c.name";
         
         model.async.forEach(tableList, function(table, callback)
