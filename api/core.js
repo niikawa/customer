685d1cc48f55a5f0c783d1802f154b8d42a7472a
niikawa <vxc.pc.niikawa@gmail.com>
2015-09-02 04:49:50 +0000

    commit

--- api/core.js
+++ api/core.js
@@ -363,12 +363,12 @@ core.prototype.isSameItemByMultipleCondition = function(conditions, callback)
 
 core.prototype.execute = function(sql, request, callback)
 {
-    this.db.connect(config, function(err)
-    {
-        if (err)
-        {
-            console.log(err);
-        }
+    // this.db.connect(config, function(err)
+    // {
+    //     if (err)
+    //     {
+    //         console.log(err);
+    //     }
         var result = [];
         var errList = [];
         console.log(sql);
@@ -398,7 +398,7 @@ core.prototype.execute = function(sql, request, callback)
         {
             callback(errList, result);
         });
-    });
+//    });
 };
 
 core.prototype.insertLog = function(userId, controlType, appendString, replace, data, callback)
