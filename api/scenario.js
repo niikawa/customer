78493da9d820e1de10505b2e8b66a7a88be285ee
niikawa <vxc.pc.niikawa@gmail.com>
2015-09-11 06:11:52 +0000

    commit

--- api/scenario.js
+++ api/scenario.js
@@ -362,6 +362,9 @@ exports.getScenarioCount = function(req, res)
     },
     function complete(err, items)
     {
+        console.log("items.count");
+        console.log(items.count);
+        
         //取得した情報から、各シナリオの登録数と登録最大数のリストを作成する
         var list = [];
         var envInfo = items.env[0];
