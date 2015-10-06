9ca78449a8e47374bf4d0564380af6846c63076c
niikawa <vxc.pc.niikawa@gmail.com>
2015-10-06 10:55:39 +0000

    commit

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ router.use(cookieParser(router.get('secretKey')));
 router.use(session({
     store: new redisStore({
         host: 'new-river.redis.cache.windows.net',
-        port: 6380,
+        port: 6379,
         pass: 'JNPUUHigCvJUUjmAHujLW9u5Hv5BNO/YV6Tj8fSLwd4=',
     }),
     secret : router.get('secretKey'),
