62f593d4be2cb9d5a7a68ba2201bd42c4d33315f
niikawa <vxc.pc.niikawa@gmail.com>
2015-10-07 09:07:51 +0000

    commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
                     dest: 'release'
                 } ]
             }
-        },        
+        },
         //オートプレフィックス
         autoprefixer: {
             target: {
